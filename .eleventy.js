1367606b1ae2feb670358f9c5af3ee3f8956565b
Peter Van Garderen <vangarderen.peter@gmail.com>
2024-04-04 21:22:58 -0700

    inital commit. 11ty + Bootstrap

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css/");
+    eleventyConfig.addWatchTarget("src/css/");
+    eleventyConfig.addPassthroughCopy("src/images/");
+    eleventyConfig.addPassthroughCopy({ "src/favicons": "/" });
+    eleventyConfig.addPassthroughCopy("src/fonts/");
+    eleventyConfig.addPassthroughCopy("src/vendor/");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
+
+    return {
+      dir: {
+        input: "src",
+        output: "docs"
+      }
+    };
+  };
