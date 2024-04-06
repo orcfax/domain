d3b989b33d9b10d0b1aa0d98b1a7d39e50da1e3f
Peter Van Garderen <vangarderen.peter@gmail.com>
2024-04-05 17:39:48 -0700

    remove underscore from assets dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/_assets/css/");
-    eleventyConfig.addWatchTarget("src/_assets/css/");
-    eleventyConfig.addPassthroughCopy("src/_assets/images/");
-    eleventyConfig.addPassthroughCopy({ "src/_assets/favicons": "/" });
-    eleventyConfig.addPassthroughCopy("src/_assets/fonts/");
-    eleventyConfig.addPassthroughCopy("src/_assets/js/");
-    eleventyConfig.addPassthroughCopy("src/_assets/vendor/");
+    eleventyConfig.addPassthroughCopy("src/assets/css/");
+    eleventyConfig.addWatchTarget("src/assets/css/");
+    eleventyConfig.addPassthroughCopy("src/assets/images/");
+    eleventyConfig.addPassthroughCopy({ "src/assets/favicons": "/" });
+    eleventyConfig.addPassthroughCopy("src/assets/fonts/");
+    eleventyConfig.addPassthroughCopy("src/assets/js/");
+    eleventyConfig.addPassthroughCopy("src/assets/vendor/");
     eleventyConfig.addPassthroughCopy("src/CNAME");
 
     return {
