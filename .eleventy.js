3e3fd89709ebed559601562ee0fa254c7b7dbc08
Peter Van Garderen <vangarderen.peter@gmail.com>
2024-04-05 17:26:13 -0700

    fix links. add images.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/css/");
-    eleventyConfig.addWatchTarget("src/css/");
-    eleventyConfig.addPassthroughCopy("src/images/");
-    eleventyConfig.addPassthroughCopy({ "src/favicons": "/" });
-    eleventyConfig.addPassthroughCopy("src/fonts/");
-    eleventyConfig.addPassthroughCopy("src/js/");
-    eleventyConfig.addPassthroughCopy("src/vendor/");
+    eleventyConfig.addPassthroughCopy("src/_assets/css/");
+    eleventyConfig.addWatchTarget("src/_assets/css/");
+    eleventyConfig.addPassthroughCopy("src/_assets/images/");
+    eleventyConfig.addPassthroughCopy({ "src/_assets/favicons": "/" });
+    eleventyConfig.addPassthroughCopy("src/_assets/fonts/");
+    eleventyConfig.addPassthroughCopy("src/_assets/js/");
+    eleventyConfig.addPassthroughCopy("src/_assets/vendor/");
     eleventyConfig.addPassthroughCopy("src/CNAME");
 
     return {
